44e62914e50fc67b93dbe0e824c0a5d127e4e62d
Breyner794 <jhoanquilindo@gmail.com>
2025-02-20 15:49:11 -0500

    Nueva carpeta para los datos y funcionalidad

---
+++ src/data/barberosData.js
@@ -0,0 +1,38 @@
+export const barberos = [
+    {
+      id: 1,
+      nombre: "Juan Pérez",
+      sede: 1,
+      imageUrl: "/src/assets/image/usuario.png",
+    },
+    {
+      id: 2,
+      nombre: "Carlos García",
+      sede: 1,
+      imageUrl: "/src/assets/image/usuario.png",
+    },
+    {
+      id: 3,
+      nombre: "Miguel López",
+      sede: 2,
+      imageUrl: "/src/assets/image/usuario.png",
+    },
+    {
+      id: 4,
+      nombre: "David Martínez",
+      sede: 2,
+      imageUrl: "/src/assets/image/usuario.png",
+    },
+    {
+      id: 5,
+      nombre: "Carlos Gomez",
+      sede: 2,
+      imageUrl: "/src/assets/image/usuario.png",
+    },
+    {
+      id: 6,
+      nombre: "Junior Castillo",
+      sede: 1,
+      imageUrl: "/src/assets/image/usuario.png",
+    },
+  ];
---
+++ src/data/servicesData.js
@@ -0,0 +1,26 @@
+export const services = [
+    {
+      id: 1, // Añadimos ID para la funcionalidad de selección
+      title: "Corte Clásico",
+      price: "$15.000",
+      duration: "30 min",
+      includes: ["Corte de cabello", "Lavado", "Peinado"],
+      icon: "✂️"
+    },
+    {
+      id: 2,
+      title: "Barba Completa",
+      price: "$10.000",
+      duration: "20 min",
+      includes: ["Perfilado", "Afeitado", "Tratamiento"],
+      icon: "💈"
+    },
+    {
+      id: 3,
+      title: "Combo Premium",
+      price: "$20.000",
+      duration: "45 min",
+      includes: ["Corte", "Barba", "Mascarilla"],
+      icon: "👑"
+    },
+  ];
