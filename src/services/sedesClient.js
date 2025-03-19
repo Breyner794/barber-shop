52db9580a6edd3ba5ae4f55b88c8ed1428501418
Breyner794 <jhoanquilindo@gmail.com>
2025-03-19 16:27:31 -0500

    Servicio para interactuar con la coleccion de sedes o sites

---
+++ src/services/sedesClient.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const getAllSite = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/site`);
+    return response.data;
+  } catch (error) {
+    console.error("error fetching sites:", error);
+    throw error;
+  }
+};
+
+export const createSite = async (siteData) => {
+    try{
+        const response = await axios.post(`${API_URL}/site`, siteData);
+      return response.data;
+    }catch (error){
+        console.error('Error creating site:', error);
+        throw error; 
+    }
+};
+
+export const updateSite = async (id, siteData) => {
+    try {
+      const response = await axios.put(`${API_URL}/site/${id}`, siteData);
+      return response.data;
+    } catch (error) {
+      console.error(`Error updating site ${id}:`, error);
+      throw error;
+    }
+  };
+
+  export const deleteSite = async (id) => {
+    try {
+      const response = await axios.delete(`${API_URL}/site/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(`Error deleting site ${id}:`, error);
+      throw error;
+    }
+  };
